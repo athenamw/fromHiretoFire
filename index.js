a819688f3fee9dadf485cd3169123a66f9141c11
athenamw <wallisathena@gmail.com>
2023-08-05 19:20:04 -0700

    added update employee function

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const mysql = require('mysql2');
 
 const db = mysql.createConnection(
@@ -13,6 +12,7 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employeeTracker_db database.`)
 );
+// List of options to select
 inquirer
   .prompt([
     {
@@ -22,6 +22,7 @@ inquirer
       choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
     },
   ])
+  // this function is for view all departments
   .then(function (answer) {
     console.log(answer);
     if (answer.viewOptions === 'view all departments') {
@@ -32,6 +33,7 @@ inquirer
         }
         console.table(data);
       });
+      // This section is to view all roles
     } else if (answer.viewOptions === 'view all roles') {
       console.log('user picked view all roles');
       db.query('SELECT * FROM role', (err, data) => {
@@ -40,6 +42,7 @@ inquirer
         }
         console.table(data);
       });
+      // This section is to view all employee info
     } else if (answer.viewOptions === 'view all employees') {
       console.log('user picked view all employees');
       db.query("select employee.first_name, employee.last_name, role.title, CONCAT(manager.first_name,' ', manager.last_name) as manager_name from employee left join role on employee.role_id = role.id left join employee manager on manager.id = employee.manager_id;", (err, data) => {
@@ -48,6 +51,7 @@ inquirer
         }
         console.table(data);
       });
+      // This section adds a department
     } else if (answer.viewOptions === 'add a department') {
       console.log('user picked add a department');
       inquirer
@@ -68,9 +72,11 @@ inquirer
             });
           });
         });
+      // This section is to add a role
     } else if (answer.viewOptions === 'add a role') {
       console.log('user picked add a role');
       inquirer
+        // These are the questions about the role
         .prompt([
           {
             type: 'input',
@@ -88,6 +94,7 @@ inquirer
             message: 'What department should this role be added to?',
           },
         ])
+        // This is to get the department id
         .then(function (roleAnswer) {
           console.log(roleAnswer);
           const deptName = [roleAnswer.newRoleDept];
@@ -96,6 +103,7 @@ inquirer
               console.log(err);
             } else {
               console.log(data);
+              // This variable takes the information from in users input
               const roleInfo = [roleAnswer.newRole, roleAnswer.newSalary, data[0].id];
               db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', roleInfo, (err, data) => {
                 if (err) {
@@ -109,9 +117,11 @@ inquirer
             }
           });
         });
+      // This section adds an employee
     } else if (answer.viewOptions === 'add an employee') {
       console.log('user picked add an employee');
       inquirer
+        // These ask the user for information about the employee
         .prompt([
           {
             type: 'input',
@@ -134,6 +144,7 @@ inquirer
             message: 'Who is the manager for the new employee?',
           },
         ])
+        // This gets the role id information
         .then(function (newEmpAns) {
           console.log(newEmpAns);
           const empRole = [newEmpAns.newEmpRole];
@@ -142,14 +153,62 @@ inquirer
               console.log(err);
             } else {
               console.log(role);
+              // This gathers the manager id information
               const empMgrName = newEmpAns.newEmpMgr.split(' ');
               db.query('SELECT * FROM employee WHERE first_name = ? and last_name = ?', empMgrName, (err, mgrData) => {
                 if (err) {
                   console.log(err);
                 } else {
                   console.log(mgrData);
+                  // This inserts the new employee into the db
                   const newEmpInfo = [newEmpAns.newEmpFirst, newEmpAns.newEmpLast, role[0].id, mgrData[0].id];
                   db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?,?)', newEmpInfo, (err, data) => {
+                    if (err) {
+                      console.log(err);
+                    } else {
+                      db.query('SELECT * FROM employee', (err, data) => {
+                        console.table(data);
+                      });
+                    }
+                  });
+                }
+              });
+            }
+          });
+        });
+    } else if (answer.viewOptions === 'update an employee role') {
+      console.log('user picked update an employee role');
+      inquirer
+        // questions to know which employee to update
+        .prompt([
+          {
+            type: 'input',
+            name: 'updateEmp',
+            message: 'Which employee do you want to update?',
+          },
+          {
+            type: 'input',
+            name: 'updateEmpRole',
+            message: 'What is the new role for this employee?',
+          },
+        ])
+        // This is to get the department id
+        .then(function (updateEmpAnswers) {
+          console.log(updateEmpAnswers);
+          const nameEmpUpdate = updateEmpAnswers.updateEmp.split(' ');
+          db.query('SELECT * FROM employee WHERE first_name = ? and last_name = ?', nameEmpUpdate, (err, empData) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(empData);
+              db.query('SELECT * FROM role WHERE title = ?', updateEmpAnswers.updateEmpRole, (err, role) => {
+                if (err) {
+                  console.log(err);
+                } else {
+                  console.log(role);
+                  // This variable takes the information from in users input
+                  const updateRoleInfo = [role[0].id, empData[0].id];
+                  db.query('UPDATE employee SET role_id = ? WHERE id = ?', updateRoleInfo, (err, data) => {
                     if (err) {
                       console.log(err);
                     } else {
